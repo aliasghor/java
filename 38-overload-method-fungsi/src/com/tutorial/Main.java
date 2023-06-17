a79186f45f03160fe5c9c562c8a2d4edbeafdb7a
alimasyhuriasghor <alimasyhuriasghor6@gmail.com>
2023-06-17 07:48:53 -0700

    Nambahin beberapa line of code

--- 38-overload-method-fungsi/src/com/tutorial/Main.java
+++ 38-overload-method-fungsi/src/com/tutorial/Main.java
@@ -2,6 +2,40 @@ package com.tutorial;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tikus");
+        print(5);
+        print(5.5f);
+        print(5.5d);
+        print(150000L);
+
+        System.out.println("Hasil pejumlahan nilai integer = " + jumlah(5,5));
+        System.out.println("Hasil pejumlahan nilai float = " + jumlah(5.5f,5));
+        System.out.println("Hasil pejumlahan nilai float = " + jumlah(5,5.5f));
+    }
+   private static float jumlah(int angka1,float angka2) {
+    return angka1 + angka2;
+   }
+
+   private static float jumlah(float angka1, int angka2) {
+    return angka1 + angka2;
+   }
+
+    private static int jumlah(int angka1,int angka2) {
+        return angka1 + angka2;
+    }
+
+    private static void print(long nilaiLong) {
+        System.out.println("Nilai long = " + nilaiLong);
+    }
+
+    private static void print(double nilaiDouble) {
+        System.out.println("Angka double = " + nilaiDouble);
+    }
+
+    private static void print(float nilaiFloat) {
+        System.out.println("Angka float = " + nilaiFloat);
+    }
+
+    private static void print(int nilaiInt) {
+        System.out.println("Angka ini adalah integer dengan nilai " + nilaiInt);
     }
 }
